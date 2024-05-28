fe57bcbad01ca607795cbdb4d049b45eed61e0e0
Hezekiah Elisha <elishahezekiah903@gmail.com>
2024-05-28 16:49:07 +0300

    :memo: Signup creation

--- ehealthhub/src/components/DashboardHeader.jsx
+++ ehealthhub/src/components/DashboardHeader.jsx
@@ -1,6 +1,59 @@
-import { Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
+import { logout } from '../redux/user/userSlice'
+import { instance } from '../api';
+import { useEffect, useState } from 'react';
 
 export default function DashboardHeader() {
+  const dispatch = useDispatch();
+  const { currentUser } = useSelector(state => state.user);
+  const navigate = useNavigate();
+  const [user, setuser] = useState({})
+
+
+  useEffect(() => {
+    if(currentUser == null){
+      navigate('/signin')
+    }
+    
+    try {
+      const config = {
+        headers: {
+          'Content-Type': 'application',
+          'Authorization': `Bearer ${currentUser.accessToken}`
+        }
+      }
+      instance.get('/user', config)
+        .then((response) => {
+            setuser(response.data)
+          }
+        )
+        .catch((error) => {
+          console.log(error)
+        })
+    } catch (error) {
+      console.log(error)
+    }
+
+  },[]);
+
+  const handleLogout = () => {
+    try{
+      const config = {
+        headers: {
+          'Content-Type': 'application',
+          'Authorization': `Bearer ${currentUser.accessToken}`
+        }
+      }
+      instance.get('/auth/logout', config)
+      dispatch(logout())
+      navigate('/signin')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+
   return (
     <nav className='w-full bg-blue-950 font-poppins static'>
       <div className="flex items-center justify-between p-4">
@@ -9,9 +62,8 @@ export default function DashboardHeader() {
           <a href="/dashboard" className="text-white">Dashboard</a>
         </div>
         <div className="flex items-center space-x-4">
-          <a href="/dashboard/profile" className="text-white">Profile</a>
-          <a href="/dashboard/settings" className="text-white">Settings</a>
-          <a href="/dashboard/logout" className="text-white">Logout</a>
+          <a href="/dashboard/profile" className="text-white">Hi, <span className='capitalize'>{user.name}</span></a>
+          <p className="text-white" onClick={handleLogout}>Logout</p>
         </div>
       </div>
     </nav>
